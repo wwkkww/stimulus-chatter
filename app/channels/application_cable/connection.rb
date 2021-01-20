9922c7e2d8760a4b37eb026144305de01d77ee5b
kevin <wongkokwah@gmail.com>
2021-01-20 10:59:39 +0800

    add session restriction to action cable connection

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,10 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :session_id
+
+    # restrict web socket communication to user session
+    def connect
+      self.session_id = request.session_id
+    end
   end
 end
