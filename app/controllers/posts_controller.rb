42d51462f586da0bfebb043d1689d4abd1ef5bd8
kevin <wongkokwah@gmail.com>
2021-01-20 10:57:42 +0800

    setup basic Post crud

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,24 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all.order(created_at: :desc)
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:success] = "Post successfully created"
+      redirect_to posts_path
+    else
+      flash[:error] = "Something went wrong"
+      render 'index'
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body)
+  end
+  
 end
